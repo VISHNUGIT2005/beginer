06063ffb1e2d86dbdbb37b3fa6095ccf63d8cb21
vishnu <vishnuraj0905@gmail.com>
2025-03-08 11:57:28 +0530

    changes 1

--- forgot password.py
+++ forgot password.py
@@ -1,5 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 driver= webdriver.Chrome()
-driver.get(https://the-internet.herokuapp.com/)
+driver.get("https://the-internet.herokuapp.com/")
+time.sleep(2)
+x=driver.find_element(By.XPATH,'//*[@id="content"]/ul/li[20]/a')
+
+x.click()
+time.sleep(3)
+y=driver.find_element(By.ID, "email")
+y.send_keys("vishnuraj0905@gmail.com")
+time.sleep(3)
+z=driver.find_element(By.XPATH,'//*[@id="form_submit"]').click()
 time.sleep(5)
